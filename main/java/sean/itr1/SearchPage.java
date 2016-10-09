d8116251cdaced5f2b7d08453ff66e3eff9be3d8
OJP5399 <ongocopj@mymail.vcu.edu>
2016-10-09 18:27:47 -0400

    MainActivity --> SearchPage
    Former Main activity page has been updated into the Search Page for the app. The Search Page will no longer be the first window the user sees when launching the app as new. Main launcher activity has been moved to the Home Page file as a result (HomePage.class). Included a new button that allows users to cancel their search and go back to Home Page.

--- main/java/sean/itr1/SearchPage.java
+++ main/java/sean/itr1/SearchPage.java
@@ -11,18 +11,19 @@ import android.content.Intent;
 import android.widget.Button;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SearchPage extends AppCompatActivity {
 
 
-    Button search,addNewSeries;
+    Button search,addNewSeries,returnHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_search_page);
 
         search = (Button)findViewById(R.id.search_confirm_basic);        //"Search" button
         addNewSeries = (Button)findViewById(R.id.add_new_series_button); //"Add A New Series" button.
+        returnHome = (Button)findViewById(R.id.return_home_button);      //"Return to Home Page" button.
     }
 
     public void onClick(View choice){
@@ -34,6 +35,11 @@ public class MainActivity extends AppCompatActivity {
         else if (choice.getId() == R.id.add_new_series_button){
             Intent createNewSeries = new Intent(this, AddNewSeries.class);
             startActivity(createNewSeries);    //If selected, brings user to the series creation screen (specified as activity_add_new_series.xml/AddNewSeries.class).
+        }
+
+        else if (choice.getId() == R.id.return_home_button){
+            Intent returnHome = new Intent(this, HomePage.class);
+            startActivity(returnHome);    //If selected, brings user to the series creation screen (specified as activity_add_new_series.xml/AddNewSeries.class).
         }
     }
 }
