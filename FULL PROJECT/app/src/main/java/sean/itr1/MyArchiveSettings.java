10d0e1bd7d84e03f45c9a4f75542555606f2f133
OJP5399 <ongocopj@mymail.vcu.edu>
2016-11-05 22:46:58 -0400

    Update MyArchiveSettings.java

--- FULL PROJECT/app/src/main/java/sean/itr1/MyArchiveSettings.java
+++ FULL PROJECT/app/src/main/java/sean/itr1/MyArchiveSettings.java
@@ -3,28 +3,28 @@ package sean.itr1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
-import android.content.Intent;
 import android.view.View;
 
 public class MyArchiveSettings extends AppCompatActivity {
 
-    Button backToMyArchiveMain, saveMyArchiveChanges;
+    Button saveMyArchiveChanges, cancelMyArchiveChanges;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_archive_settings);
 
-        backToMyArchiveMain = (Button)findViewById(R.id.back_to_myArchiveMain);
-        saveMyArchiveChanges = (Button)findViewById(R.id.save_myArchive_settings);
+        saveMyArchiveChanges = (Button)findViewById(R.id.save_changes_button);
+        cancelMyArchiveChanges = (Button)findViewById(R.id.cancel_changes_button);
     }
 
     public void onClick(View choice){
-        if(choice.getId() == R.id.back_to_myArchiveMain){
+        if (choice.getId() == R.id.cancel_changes_button){
+            //No code needed here: This button will send the user back to the previous screen without making adjustments to MyArchive Page.
             finish();
         }
-        else if (choice.getId() == R.id.save_myArchive_settings){
-            //Add code here that will save the changes made to user's MyArchive.
+        else if (choice.getId() == R.id.save_changes_button){
+            //Add code here that will save user changes to My Archive and send a broadcast intent stating so.
             finish();
         }
     }
